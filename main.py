6d17c7e75774ca5eda0db59ac70d1aaf30b93d4e
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 11:51:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from pyrogram import Client ,idle
+from config import *
+
+plugins = dict(root="plugins")
+
+bot=Client( 
+    "teInvi",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=plugins)
+
+bot.start()
+uname = (bot.get_me()).username
+a = f"""
+`╭━━╮╱╱╱╭━━━┳╮
+╰┫┣╯╱╱╱┃╭━╮┃┃
+╱┃┃╭╮╭╮┃┃╱┃┃┃╭┳╮╭┳━━╮
+╱┃┃┃╰╯┃┃╰━╯┃┃┣┫╰╯┃┃━┫
+╭┫┣┫┃┃┃┃╭━╮┃╰┫┣╮╭┫┃━┫
+╰━━┻┻┻╯╰╯╱╰┻━┻╯╰╯╰━━╯`
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+➖➖➖➖➖➖➖➖➖➖
+@{uname} has been deployed!
+➖➖➖➖➖➖➖➖➖➖
+Support: @AlphaTm_Botz_chat
+➖➖➖➖➖➖➖➖➖➖
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+"""
+
+print(a)
+async def log():
+    if LOG_CHANNEL:
+       await bot.send_message(LOG_CHANNEL, text=a)
+       
+
+bot.run(log())
+idle()

c6015f9ae8bb6ae82e374b6e59e45bdfc59f6587
Gishan Krishka <96438111+Gishankrishka2@users.noreply.github.com>
2022-07-11 11:54:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 API_ID = int(os.getenv("API_ID", "19611094")) 
-API_HASH = os.getenv("API_HASH", "c5198b0dab5cdd8e0eaaf3e0c742fbd3")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "5494219369:AAFk5iMvpwjvWi3MoKXcNYQuBHa5wJ-_qMY")
+API_HASH = os.getenv("API_HASH", "c5hjk;hljkl;lkjlkjkhlkjklhjkljhkjklhjk")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001689365631"))
-ADMIN=[1884885842 ,5025877489]
-MONGO = 'mongodb+srv://epic:epic@cluster0.257eaxu.mongodb.net/?retryWrites=true&w=majority'
